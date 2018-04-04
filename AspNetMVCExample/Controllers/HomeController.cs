9718b88315aee3f9721451df6eb03b25669351e8
lucasalzone <luca.salzone@gmail.com>
2018-04-05 00:16:54 +0200

    Esempi Utilizzo ViewBag

--- AspNetMVCExample/Controllers/HomeController.cs
+++ AspNetMVCExample/Controllers/HomeController.cs
@@ -7,19 +7,29 @@ using System.Web.Mvc;
 namespace AspNetMVCExample.Controllers {
     public class HomeController : Controller {
         public ActionResult Index() {
+            ViewBag.Oggetto = new Contatto("Luca","Salzone");
             return View();
         }
 
         public ActionResult About() {
             ViewBag.Message = "Your application description page.";
-
+            ViewBag.Oggetto = new Contatto("Luca","Salzone");
             return View();
         }
 
         public ActionResult Contact() {
             ViewBag.Message = "Your contact page.";
-
+            ViewBag.Oggetto = new Contatto("Luca","Salzone");
             return View();
+        }
+    }
+
+    public class Contatto{
+        public string Nome{get;}
+        public string Cognome{get;}
+        public Contatto(string nome, string cognome){
+            this.Nome=nome;
+            this.Cognome=cognome;
         }
     }
 }
